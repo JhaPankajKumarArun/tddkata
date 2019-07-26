7bd5b3872c2d9ab79b8c24d54f13f062a7fc5986
Pankaj Kumar Jha <jha.programmer@gmail.com>
2019-07-26 16:06:29 +0530

    Adding StringCalculatorSpec.groovy into test folder

---
+++ TDDTrainingDemo/src/test/java/test/StringCalculatorSpec.groovy
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+import spock.lang.Specification
+
+class StringCalculatorSpec extends Specification {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
