31c66242220692001db3e815efb014a9ec5b12b7
Pankaj Kumar Jha <jha.programmer@gmail.com>
2019-07-26 16:21:09 +0530

    Changed Build path JRE to JDK and add testcase

--- TDDTrainingDemo/src/test/java/test/StringCalculatorSpec.groovy
+++ TDDTrainingDemo/src/test/java/test/StringCalculatorSpec.groovy
@@ -8,9 +8,12 @@ import spock.lang.Specification
 
 class StringCalculatorSpec extends Specification {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	def'API to calculate string'(){
+		given: 'Empty string'
+			def argument=""
+		when: 'StringCalculator.add is called'
+			def retVal = StringCalculator.add(argument);
+		then: 'It should return zero'
+			retVal=="0"
 	}
-
 }
